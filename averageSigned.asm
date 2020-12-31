1fb5afd32f854d9d7b6c38fd8b89f3b801a11ba9
Ecem Balıkçı <balikci8ecem@gmail.com>
2020-12-31 12:08:01 +0300

    added some comments

--- averageSigned.asm
+++ averageSigned.asm
@@ -2,29 +2,29 @@
 .STACK 100
 .DATA        
 
-SIGN_DATA DB +25, -78, -95, +22, -69, +25, -85, +47, -39
-SUM DW ?
-AVERAGE DB ?
+SIGN_DATA DB +25, -78, -95, +22, -69, +25, -85, +47, -39  
+SUM DW ?  ; stored in DX
+AVERAGE DB ? ; stored in AL
 .CODE  
 MOV AX,@DATA
 MOV DS,AX 
 XOR AX,AX             
   MOV BX, 9             ; # of elements in array
   XOR DX, DX            ; clear dx to store sum
-  MOV SI, OFFSET SIGN_DATA  
+  MOV SI, OFFSET SIGN_DATA  ; LEA SI, SIGN_DATA
 AGAIN:
   MOV AL, [SI]          ; next byte element
   CBW                  
   ADD DX, AX            ; add to the sum           
-  MOV SUM,DX            ; store dx in SUM
+  MOV SUM,DX            ; store DX in SUM
          
-  INC SI                ; To next element of array
-  DEC BX
+  INC SI                ; to next element of array
+  DEC BX                ; decrement bx since it acts like a counter
   JNZ AGAIN 
-  MOV AX,DX
+  MOV AX,DX             ; store the sum in ax to divide it afterwards
   MOV BL,9 
-  IDIV BL
+  IDIV BL               ; divide sum into # of elements in array
   MOV AVERAGE,AL
-MOV AX,4C00H
+MOV AX,4C00H            ; exit
 INT 21H
 END 
